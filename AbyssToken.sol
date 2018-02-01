e591a7195c9b3c288046f1324b2c9f9c18066cf8
slivlen <avkobelev@gmail.com>
2018-02-01 16:59:04 +0300

    AbyssToken constructor name fix

--- AbyssToken.sol
+++ AbyssToken.sol
@@ -4,7 +4,7 @@ import './token/TransferLimitedToken.sol';
 contract AbyssToken is TransferLimitedToken {
     uint256 public constant SALE_END_TIME = 1522540800; // 01.04.2018
 
-    function TheAbyssToken(address _listener, address[] _owners, address manager) public
+    function AbyssToken(address _listener, address[] _owners, address manager) public
         TransferLimitedToken(SALE_END_TIME, _listener, _owners, manager)
     {
         name = "ABYSS";
