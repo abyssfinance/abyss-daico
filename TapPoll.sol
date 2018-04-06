b31a0f2172d10c9343c5a3b644a584e74b409683
slivlen <avkobelev@gmail.com>
2018-04-06 19:22:51 +0300

    Intial tap 500 ETH per month, max tap increase is now limited by 50%

--- TapPoll.sol
+++ TapPoll.sol
@@ -9,7 +9,8 @@ import './fund/IPollManagedFund.sol';
  * @dev Poll to increase tap amount
  */
 contract TapPoll is BasePoll {
-    uint256 tap;
+    uint256 public tap;
+    uint256 public minTokensPerc = 0;
 
     /**
      * TapPoll constructor
@@ -18,15 +19,32 @@ contract TapPoll is BasePoll {
      * @param _fundAddress Fund contract address
      * @param _startTime Poll start time
      * @param _endTime Poll end time
+     * @param _minTokensPerc - Min percent of tokens from totalSupply where poll is considered to be fulfilled
      */
-    function TapPoll(uint256 _tap, address _tokenAddress, address _fundAddress, uint256 _startTime, uint256 _endTime) public
+    function TapPoll(
+        uint256 _tap,
+        address _tokenAddress,
+        address _fundAddress,
+        uint256 _startTime,
+        uint256 _endTime,
+        uint256 _minTokensPerc
+    ) public
         BasePoll(_tokenAddress, _fundAddress, _startTime, _endTime, false)
     {
         tap = _tap;
+        minTokensPerc = _minTokensPerc;
     }
 
     function onPollFinish(bool agree) internal {
         IPollManagedFund fund = IPollManagedFund(fundAddress);
         fund.onTapPollFinish(agree, tap);
+    }
+
+    function getVotedTokensPerc() public view returns(uint256) {
+        return safeDiv(safeMul(safeAdd(yesCounter, noCounter), 100), token.totalSupply());
+    }
+
+    function isSubjectApproved() internal view returns(bool) {
+        return yesCounter > noCounter && getVotedTokensPerc() >= minTokensPerc;
     }
 }
